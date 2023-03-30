3630ab650b4a9e59707aa004e2bf65e9787d12c4
MazzoliMW <riccardo.mazzoli@marketwall.com>
2023-03-30 12:01:05 +0200

    A

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -17,23 +17,23 @@ function App(props) {
   return (    
     <div className="App">
       <header className="App-header">
-      <h1>TodoMatic</h1>
-      <Form />
-      <div className="spacer">
-          <div className="filters btn-group stack-exception">
-            <FilterButton name="All" style="btn toggle-btn btn-dark"/>
-            <FilterButton name="Active" style="btn toggle-btn btn-secondary"/>
-            <FilterButton name="Completed" style="btn toggle-btn btn-light"/>
-          </div> 
-      </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-exception"
-        aria-labelledby="list-heading"
-      >
-        {taskList}
-      </ul>
+        <h1>TodoMatic</h1>
+        <Form />
+        <div className="spacer">
+            <div className="filters btn-group stack-exception">
+              <FilterButton name="All" style="btn toggle-btn btn-dark"/>
+              <FilterButton name="Active" style="btn toggle-btn btn-secondary"/>
+              <FilterButton name="Completed" style="btn toggle-btn btn-light"/>
+            </div> 
+        </div>
+        <h2 id="list-heading">3 tasks remaining</h2>
+        <ul
+          role="list"
+          className="todo-list stack-exception"
+          aria-labelledby="list-heading"
+        >
+          {taskList}
+        </ul>
       </header>
     </div>
   );
